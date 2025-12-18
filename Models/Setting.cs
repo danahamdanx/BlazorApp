7b60b3dc7a3ecdb161ad177e9ef66e1b60dddcd2
danahamdanx <dana29454@gmail.com>
2025-12-18 17:10:01 +0200

    task5 code

---
+++ Models/Setting.cs
@@ -0,0 +1,9 @@
+namespace Client.Models;
+
+public class Setting
+{
+    public int Id { get; set; }
+    public string SettingKey { get; set; }
+    public string SettingValue { get; set; }
+    public string Description { get; set; }
+}
